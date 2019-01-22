8eb43bc941f15088dd4674bfb94fc055e0bfdfc8
BB20101997 <bb-github@t-online.de>
2019-01-22 20:16:58 +0100

    Revert "moved to a macro for implementing the conection_init function twice"
    This reverts commit ffe98a171c5589b1f37f49aced15b018cbbe236f.

--- network/src/network/connection.rs
+++ network/src/network/connection.rs
@@ -21,118 +21,83 @@ pub enum Connection {
     Client(Client),
 }
 
+
 pub mod init {
-    use super::*;
+    use lib::prelude::v1::*;
     use lib::part;
-
-    pub use super::Side;
-
-    type A = Side;
-    type B = Side;
-    type C = Connection;
-    type D = Connection;
+    use crate::network::connection::Side;
+    pub use crate::network::connection::Side::*;
+    use std::io::Result;
+    use crate::network::connection::Connection;
+    use std::net::TcpStream;
+    use std::io::Write;
 
     const USERNAME_QUESTION: &str = "Your name, please: ";
 
-    macro_rules! choose {
-        (server $client:tt $server:tt) => {
-            $server
-        };
-        (client $client:tt $server:tt) => {
-            $server
-        };
-    }
+    pub fn connection_init(side: Side, mut stream: TcpStream) -> Result<Connection> {
 
-    macro_rules! dup {
-        () => {
-            dup!(server connection_init_server C);
-            dup!(client connection_init_client D);
-        };
-        ($side:tt $name:ident $tp:ty) => {
-            pub fn $name(side: choose!($side  A B), mut stream: TcpStream) -> Result<$tp> {
-                let stream_copy = &mut part!( stream in? Ok(stream)  =  stream.try_clone());
+        let stream_copy = &mut part!( stream in? Ok(stream)  =  stream.try_clone());
 
-                let mut read = ReadWrite::READ { reader: &mut stream };
-                let mut write = ReadWrite::WRITE { writer: stream_copy };
+        let mut read = ReadWrite::READ { reader: &mut stream };
+        let mut write = ReadWrite::WRITE { writer: stream_copy };
 
-                let to_client;
-                let to_server;
+        let to_client;
+        let to_server;
 
-
-                choose!($side
-                    {
-                        to_client = &mut read;
-                        to_server = &mut write;
-                        println!("Connection Successful");
-                    }
-                    {
-                        to_server = &mut read;
-                        to_client = &mut write;
-                        println!("Connection received")
-                    }
-                );
-
-
-                let mut username;
-
-                choose!($side
-                    {
-
-                    username = part!(name try until Ok(name) = ask(USERNAME_QUESTION));
-                    println!("Sending Username ...");
-                    }
-                    {
-
-                    username = String::new();
-                    println!("Expecting Username ...");
-                    }
-                );
-
-                username.serialize(to_server)?;
-
-                if false {
-                    let mut test = Test { name: "Wrong".to_string(), age: -42 };
-                    choose!($side
-                        {
-                            test.name = "Correct".to_string();
-                            test.age = 42;
-                        }
-                        {
-
-                        }
-                    );
-
-                    test.serialize(to_server)?;
-
-                    choose!($side
-                        {
-
-                        }
-                        {
-                            println!("Test{{name: {}, age: {}}}", test.name, test.age);
-                        }
-                    );
-
+        {
+            match side {
+                Server(..) => {
+                    to_server = &mut read;
+                    to_client = &mut write;
+                    println!("Connection received")
                 }
-
-                stream.flush().unwrap();
-
-                 choose!($side
-                    {
-                        use lib::api::structs;
-                        Ok(Connection::Client(structs::Client { stream, commands: Vec::new() }))
-
-                    }
-                    {
-                        println!("New User {}", &username);
-                        Ok(Connection::Server(ServerConnection { stream, username }))
-
-                    }
-                )
+                Client(..) => {
+                    to_client = &mut read;
+                    to_server = &mut write;
+                    println!("Connection Successful");
+                }
             }
-        };
+        }
+
+        let mut username = String::new();
+
+
+
+
+        if let Client(..) = side{
+            username = part!(name try until Ok(name) = ask(USERNAME_QUESTION));
+            println!("Sending Username ...");
+        }else{
+            println!("Expecting Username ...");
+        }
+
+        username.serialize( to_server)?;
+
+        if false {
+            let mut test = Test { name: "Wrong".to_string(), age: -42 };
+
+            if let Client(..) = side {
+                test.name = "Correct".to_string();
+                test.age = 42;
+            }
+
+            test.serialize(to_server);
+
+            if let Server(..) = side {
+                println!("Test{{name: {}, age: {}}}", test.name, test.age);
+            }
+
+
+        }
+
+        stream.flush().unwrap();
+
+        if let Server(_) = side {
+            println!("New User {}", &username);
+            Ok(Connection::Server(ServerConnection{stream,username}))
+        }else{
+            use  lib::api::structs;
+            Ok(Connection::Client(structs::Client{stream,commands:Vec::new()}))
+        }
     }
-
-    dup!();
-
 }
